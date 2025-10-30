b3e36fe01d9279273b33be6c20af9ea57bc6f34a
Sayan520 <ghoshsayan5205@gmail.com>
2025-10-30 12:51:22 +0530

    candidate_details modified

--- controllers/ProfileMicroservices/candidate_details.py
+++ controllers/ProfileMicroservices/candidate_details.py
@@ -16,7 +16,7 @@ def candidate_details():
             }), 400
 
         email = data["email"]
-        jobId = data.get("jobId")  # Make jobId optional using `get()`
+        jobId = data.get("jobId")  # Make jobId optional using get()
 
         conn = get_db_connection()
         cursor = conn.cursor(dictionary=True)
@@ -64,6 +64,37 @@ def candidate_details():
         else:
             # If no jobId is provided, set match_percentage to None
             candidate["match_percentage"] = None
+
+
+        # #  Fetch AI Screening Score & Details
+        #     cursor.execute("""
+        #         SELECT score, question_answer
+        #         FROM assessment_session_log
+        #         WHERE candidateId = %s AND jobId = %s
+        #     """, (candidate_id, jobId))
+        #     aiscreening = cursor.fetchone()
+        #     if aiscreening:
+        #         candidate["aiscreening_score"] = aiscreening["score"]
+        #         candidate["screening_details"] = aiscreening["question_answer"]
+
+        # #  Fetch Assessment Score & Details (with JOIN)
+        #     cursor.execute("""
+        #         SELECT 
+        #             ja.score AS assessment_score,
+        #             af.details AS assessment_details
+        #         FROM jobassessments ja
+        #         JOIN assessmentinfo af 
+        #             ON s.candidateId = d.candidateId AND s.jobId = d.jobId
+        #         WHERE s.candidateId = %s AND s.jobId = %s
+        #     """, (candidate_id, jobId))
+
+        #     assessment = cursor.fetchone()
+        #     if assessment:
+        #         candidate["assessment_score"] = assessment["assessment_score"]
+        #         candidate["assessment_details"] = assessment["assessment_details"]
+
+
+
 
         # Step 3: Return response with candidate details
         return jsonify({
